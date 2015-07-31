2a2e0d6c3e07933125994397e124f98322a9979e
Fadi Eastline PC <fzahhar@eastlinemarketing.com>
2015-07-31 18:07:16 +0300

    adding the tuts plus oop plugin development course

---
+++ tutsplus-post-notice/class-tutsplus-post-notice.php
@@ -0,0 +1,47 @@
+<?php
+
+class TutsPlus_Post_Notice {
+
+  public function __construct($editor) {
+    $editor->initialize();
+  }
+
+  public function initialize() {
+    add_action('admin_enqueue_scripts', array($this,'enqueue_styles'));
+    add_action('admin_enqueue_scripts', array($this,'enqueue_scripts'));
+  }
+
+  public function enqueue_styles() {
+
+    $screen = get_current_screen();
+    if ( 'post' != $screen->id ) {
+      return;
+    }
+    wp_enqueue_style(
+    'tutsplus-post-notice-admin',
+    plugins_url('tutsplus-post-notice/assets/css/admin.min.css' ),
+    array(),
+    '0.1.0'
+  );
+
+  }
+
+
+  public function enqueue_scripts() {
+
+    $screen = get_current_screen();
+    if ( 'post' != $screen->id ) {
+      return;
+    }
+
+    
+    wp_enqueue_script(
+    'tutsplus-post-notice-admin',
+    plugins_url('tutsplus-post-notice/assets/js/admin.min.js' ),
+    array('jquery'),
+    '0.1.0'
+  );
+
+
+  }
+}
